42ddbb0340746ef0a709b8cf1bac6bd20857438d
winmanss <Parkmoosung090526@gmail.com>
2025-08-06 16:17:25 +0900

    Add files via upload

---
+++ 이름없음2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int num1 = 10;
+	int num2, num3;
+	num2 = 20;
+	num3 = 30;
+	char ch;
+	ch = 'A';
+	
+	printf("num1 °ª: %d \n", num1);
+	printf("num2 °ª: %d \n", num2, num3);
+	printf("ch°ª: %c \n", ch);
+	
+	getchar();
+	return 0;
+}
